b3f0b4bc1f470604e2e958dee9b7836b7d7cd580
Adil omer <36453919+adelazhary@users.noreply.github.com>
2024-08-05 07:02:56 +0800

    first commit

---
+++ app/Livewire/DonationRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class DonationRequest extends Component
+{
+    public function render()
+    {
+        return view('livewire.donation-request');
+    }
+}
